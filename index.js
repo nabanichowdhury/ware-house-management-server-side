8eede9a882578180786f803dd8bbbf83e8a922e4
nabanichowdhury <nabanichowdhury@gmail.com>
2022-05-08 21:04:36 +0530

    jwt add

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -7,8 +14,26 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // midddleware
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!auth) {
+    return res.status(401).send({ message: "unaothorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403), send({ message: "forbodden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+  });
+  console.log("inside verifyJWT", authHeader);
+  next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8joe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -23,6 +48,18 @@ async function run() {
     const inventoryCollection = client
       .db("wareHouse")
       .collection("inventories");
+
+    //   AUTH
+    app.get("/login", async (req, res) => {
+      const user = req.body;
+
+      console.log(user);
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send(accessToken);
+    });
+
     //    get all the data in api
     app.get("/inventory", async (req, res) => {
       const query = {};
@@ -43,6 +80,23 @@ async function run() {
         const newInventory = req.body;
         const result = await inventoryCollection.insertOne(newInventory);
         res.send(result);
+      });
+      //  GetMYItems
+      app.get("/myitems", verifyJWT, async (req, res) => {
+        const authHeader = req.headers.authorization;
+        console.log(authHeader);
+        const decodedEmail = req.query.email;
+        const email = req.query.email;
+
+        if (email == decodedEmail) {
+          const query = { email: email };
+
+          const cursor = inventoryCollection.find(query);
+          const items = await cursor.toArray();
+          res.send(items);
+        } else {
+          res.status(403).send({ message: "forbidden access" });
+        }
       });
 
       //   Update the quantity
@@ -71,15 +125,6 @@ async function run() {
         const query = { _id: ObjectId(id) };
         const result = await inventoryCollection.deleteOne(query);
         res.send(result);
-      });
-
-      //  GetMYItems
-      app.get("/myitem", async (req, res) => {
-        const email = req.query;
-        const query = { email: email };
-        const cursor = inventoryCollection.find(query);
-        const items = await cursor.toArray();
-        res.send(items);
       });
     });
   } finally {
